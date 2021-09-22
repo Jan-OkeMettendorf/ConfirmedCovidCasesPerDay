304918aa0673039d232a91f8a67821dc6ef4d86a
Jan-Oke Mettendorf <jan-oke-mettendorf@web.de>
2021-09-22 15:53:24 +0200

    Add new project

---
+++ src/main/java/de/neuefische/confirmedcovidcasesperday/ConfirmedCovidCasesPerDayApplication.java
@@ -0,0 +1,13 @@
+package de.neuefische.confirmedcovidcasesperday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfirmedCovidCasesPerDayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfirmedCovidCasesPerDayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/neuefische/confirmedcovidcasesperday/ConfirmedCovidCasesPerDayApplicationTests.java
@@ -0,0 +1,13 @@
+package de.neuefische.confirmedcovidcasesperday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfirmedCovidCasesPerDayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
